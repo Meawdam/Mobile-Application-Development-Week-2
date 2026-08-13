f8e89cd5015bf632afeb958019eb982e2384ba12
DarkCat <meawdamxza@gmail.com>
2026-08-13 23:03:07 +0700

    Initialize mobile application with basic structure and configuration files

---
+++ bin/mobile_application_week_2.dart
@@ -0,0 +1,5 @@
+import 'package:mobile_application_week_2/mobile_application_week_2.dart' as mobile_application_week_2;
+
+void main(List<String> arguments) {
+  print('Hello world: ${mobile_application_week_2.calculate()}!');
+}
---
+++ test/mobile_application_week_2_test.dart
@@ -0,0 +1,8 @@
+import 'package:mobile_application_week_2/mobile_application_week_2.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
